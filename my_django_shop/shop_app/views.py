269bc8d30fed70f78f18f68b90d37e0aeaff62da
villian2008 <villian2008@yandex.ru>
2017-03-03 23:38:23 +0800

    create first view and templates

--- my_django_shop/shop_app/views.py
+++ my_django_shop/shop_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, World!!!')
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'shop_app/index.html', context)

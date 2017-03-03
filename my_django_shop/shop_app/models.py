8463360fa18b9d185749af56f1b76532500e9768
villian2008 <villian2008@yandex.ru>
2017-03-03 22:07:47 +0800

    create model Category

--- my_django_shop/shop_app/models.py
+++ my_django_shop/shop_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=254, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    parent = models.ForeignKey('self', verbose_name='Родительская категория', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
